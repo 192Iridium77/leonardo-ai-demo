a9c9579737a030d03ce454ecc647b9f2b122f30d
Matt Martin <matt.martin0108@gmail.com>
2024-04-09 09:58:19 +0930

    protect homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
 import { Heading, Text } from "@chakra-ui/react";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
+  const session = await getServerSession();
+  if (!session) redirect("/login");
+
   return (
     <main>
       <Heading my={4}>Home</Heading>
